24c51ecca454db08841007de04ac3a43c53a1f49
jhad7010 <jhad7010@gmail.com>
2017-01-21 09:01:17 -0600

    Confirmando commit 1a prueba

--- PruebaGit4/ViewController.swift
+++ PruebaGit4/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("esta es la primera prueba en git4")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
